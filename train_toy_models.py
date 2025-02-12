d42e666982101b1d06fe86155871cddf8fde6e55
salzhu <163059861+salzhu@users.noreply.github.com>
2025-02-11 21:21:52 -0800

    Create train_toy_models.py

---
+++ train_toy_models.py
@@ -0,0 +1,117 @@
+"""
+Trains a bunch of toy models with different data orderings on the same dataset (TinyStories)
+N = number of models to train, and can change N_TRAIN_SAMPLES. 
+Can change the model architecture with config @ Line 81. 
+Can change dataset @ Line 76.
+
+For N models,
+- Shuffles the dataset for training
+- Calls "train_tiny" which takes in texts, model config, and place to save (need to pass for eval to work). Trains the model for one epoch.
+- Calls "eval", which takes the model save path and evaluates PPLX on given texts (in this case, the eval texts are the same as the train texts)
+
+For each model, PPLX on all sequences, and the data ordering are saved at DF_PATH. 
+
+Main function takes in a seed as input
+"""
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, LlamaForCausalLM, LlamaConfig
+from datasets import load_dataset
+import random
+import numpy as np
+import evaluate
+import pandas as pd
+from tqdm import tqdm
+import os
+import sys
+
+N = 20
+N_TRAIN_SAMPLES = 100000
+
+def train_tiny(train_texts, config, tokenizer, save_dir):
+    model = LlamaForCausalLM(config)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+    train_dataloader = DataLoader(train_texts, batch_size=1, shuffle=False) # assume train_texts is shuffled in desired order
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    model.train()
+
+    batch_iterator = tqdm(train_dataloader)
+
+    for batch in batch_iterator:
+        
+        inputs = tokenizer(batch, padding=True, truncation=True, return_tensors="pt")
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+
+        inputs['labels'] = inputs['input_ids'].clone()
+
+        outputs = model(**inputs)
+
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    if save_dir is not None:
+        model.save_pretrained(save_dir)
+
+def eval(model_path, eval_texts):
+    perplexity = evaluate.load("perplexity", module_type="metric")
+    eval = perplexity.compute(model_id=model_path,
+                                add_start_token=True,
+                                predictions=eval_texts)
+    pplx = np.log(eval['perplexities'])
+
+    return pplx
+
+if __name__ == "__main__":
+
+    INDEX = sys.argv[1] 
+    random.seed(INDEX)
+
+    REF_PATH = f'/nlp/u/salzhu/blackbox-model-tracing/train_references/tiny_ref_model_{INDEX}'
+    DF_PATH = f'/nlp/u/salzhu/blackbox-model-tracing/train_references/tinystories_refmodels_{INDEX}.csv'
+
+    if os.path.exists(DF_PATH):
+        df = pd.read_csv(DF_PATH)
+    else:
+        df = pd.DataFrame({})
+
+    dataset = load_dataset("roneneldan/TinyStories")
+
+    tokenizer = AutoTokenizer.from_pretrained("huggyllama/llama-7b")
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.save_pretrained(REF_PATH)
+
+    config = LlamaConfig(
+        vocab_size=tokenizer.vocab_size,
+        hidden_size=256,
+        intermediate_size=512,
+        num_hidden_layers=4,
+        num_attention_heads=8,
+        max_position_embeddings=512,
+        rms_norm_eps=1e-6,
+    )
+
+    texts = dataset["train"]["text"][:N_TRAIN_SAMPLES]
+    texts = [item for item in texts if item != ""] # some sequences are bad
+
+    for i in range(N):
+
+        print(f"Training model {i}...")
+
+        shuffle_order = list(range(len(texts)))
+        random.shuffle(shuffle_order)
+        df[f'order-{i}'] = shuffle_order
+
+        shuffled_texts = [texts[i] for i in shuffle_order]
+    
+        train_tiny(shuffled_texts, config, tokenizer, REF_PATH)
+        pplx = eval(REF_PATH, texts)
+        df[f'pplx-{i}'] = pplx
+
+        df.to_csv(DF_PATH)
