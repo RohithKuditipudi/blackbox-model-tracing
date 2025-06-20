0803581f64087f6242dedd2f9e0212413295f0ef
salzhu <163059861+salzhu@users.noreply.github.com>
2025-06-19 22:57:25 -0700

    add metric for only n-gram matches that appear once

--- tracing/sample/metrics.py
+++ tracing/sample/metrics.py
@@ -12,3 +12,13 @@ def stratified_avg(matched_text_to_steps):
                 sampled_values.append(np.random.choice(matched_text_to_steps[text][pos]))
     
     return np.mean(sampled_values)
+
+def single_match(matched_text_to_steps):
+    single_values = []
+    for text in matched_text_to_steps:
+        for pos in text:
+            if pos:
+                if len(matched_text_to_steps[text][pos]) == 1:
+                    single_values.append(matched_text_to_steps[text][pos][0])
+    
+    return np.mean(single_values)
