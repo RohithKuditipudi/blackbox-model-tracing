a96b9ec62ff72584a7d17dab868dd9306bc3190b
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2025-08-26 15:39:54 -0700

    default num jobs should be higher

--- scripts/observation/launch_tinystories.py
+++ scripts/observation/launch_tinystories.py
@@ -10,7 +10,7 @@ def main():
 
     parser.add_argument("--save_dir", type=str, required=True)
     parser.add_argument("--log_dir", type=str, default="./slurm_logs")
-    parser.add_argument("--num_jobs", type=int, default=10)
+    parser.add_argument("--num_jobs", type=int, default=10000)
     parser.add_argument("--start_job", type=int, default=0)
     parser.add_argument("--dry_run", action="store_true", help="Print commands without running them.")
 
