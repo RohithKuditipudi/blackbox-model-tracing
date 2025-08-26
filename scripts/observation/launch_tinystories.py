bc64704f6cf819361933bfdb9c72b834d614e7d5
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2025-08-26 15:49:04 -0700

    fix launching

--- scripts/observation/launch_tinystories.py
+++ scripts/observation/launch_tinystories.py
@@ -35,19 +35,21 @@ def main():
     param_values = [sweep_configs[name] for name in param_names]
     param_combinations = list(itertools.product(*param_values))
 
+    log_dir = os.path.join(args.log_dir, args.save_dir)
+
     if not args.dry_run:
         os.makedirs(args.save_dir, exist_ok=True)
-        os.makedirs(args.log_dir, exist_ok=True)
+        os.makedirs(log_dir, exist_ok=True)
 
     for i, params in enumerate(param_combinations[args.start_job:args.start_job+args.num_jobs]):
 
-        cmd = build_cmd(args=dict(zip(param_names, params)), log_path=os.path.join(args.log_dir, f"job_{i}.out"))
+        cmd = build_cmd(args=dict(zip(param_names, params)), log_path=os.path.join(log_dir, f"job_{i}.out"))
 
         print(f"Launching experiment {i+1}/{len(param_combinations)}")
         print(f"Parameters: {dict(zip(param_names, params))}")
         print("Command:", cmd)
         if not args.dry_run:
-            subprocess.run(cmd, check=True)
+            subprocess.run(cmd, shell=True,check=True)
 
 if __name__ == "__main__":
     main()
