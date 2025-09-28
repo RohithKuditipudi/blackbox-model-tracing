34f0cbc8356f1a3464b03b8e2cad25127b09a623
salzhu <163059861+salzhu@users.noreply.github.com>
2025-09-28 15:17:15 -0700

    spearman step vs. n-gram count (\phi_obs^part)

--- tracing/sample/metrics.py
+++ tracing/sample/metrics.py
@@ -1,8 +1,17 @@
 import numpy as np
+import scipy 
 from ..utils import flatten_list
 
 def avg(matched_text_to_steps):
     return np.mean(flatten_list(matched_text_to_steps))
+
+def spearman_matches(n_steps, matched_texts_to_steps):
+    counts = np.zeros(n_steps)
+    matched_text_to_steps = flatten_list(matched_texts_to_steps)
+    for step in matched_text_to_steps:
+        counts[step] += 1
+        
+    return scipy.stats.spearmanr(np.arange(n_steps), counts)
 
 def stratified_avg(matched_text_to_steps):
     sampled_values = []
@@ -22,3 +31,4 @@ def single_match(matched_text_to_steps):
                     single_values.append(matched_text_to_steps[text][pos][0])
     
     return np.mean(single_values)
+
