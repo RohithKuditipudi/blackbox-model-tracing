77cc666dfc28ad3681c3c499b95a1216c3ab917e
Jing Huang <hij@stanford.edu>
2025-09-30 10:41:43 -0700

    Allow using pre-computed metrics or other datasets.

--- scripts/query/run_query_test.py
+++ scripts/query/run_query_test.py
@@ -1,20 +1,29 @@
 """
-Prints p-value from \phi_{query}^{ref} (see Equation 2). 
-Command-line argument with
+Computes p-value from \phi_{query}^{ref} (see Equation 2).
+
+Command-line arguments
   model (\mu_\beta): HuggingFace model ID
   ref_model (\mu_0): HuggingFace model ID
   n_samples (n): Number of sequences to sample.
-  dataset_name: The name of the pre-training dataset.
-      Must be one of the subset in the following dataset:
-      https://huggingface.co/datasets/hij/sequence_samples.
+  transcript: Name or path to a HuggingFace dataset that contains ordered
+      training data samples, i.e., with an `index` and a `tokens` column.
+      For example, the transcript could be any of the subset here
+      https://huggingface.co/datasets/hij/sequence_samples, which are the
+      transcripts used in our experiment.
+  metric_column_name: If specified, use the precomputed metrics stored at
+      the given column.
+  ref_metric_column_name: If specified, use the precomputed metrics stored at
+      the given column as references.
 
-Example:
+Example usage:
 python blackbox-model-tracing/scripts/query/run_query_test.py \
     --model EleutherAI/pythia-6.9b-deduped \
     --ref_model EleutherAI/pythia-6.9b \
     --n_samples 100000 \
-    --dataset_name pythia_deduped_100k
+    --transcript hij/sequence_samples/pythia_deduped_100k
 
+This tests whether EleutherAI/pythia-6.9b-deduped is trained on
+the Pile deduped dataset.
 It takes about 3 hrs to compute the logprob of the sequences on an A100.
 The program should output a p-value around 1e-50.
 """
@@ -24,36 +33,69 @@ sys.path.append('blackbox-model-tracing')
 
 
 import argparse
+import numpy as np
 
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from tracing.index import DocumentIndex
 from tracing.query.metrics import pplx 
 from tracing.query.statistics import BasicStatistic
 from transformers import AutoTokenizer
 
 
-def phi_qr(args, document_index):
-  phi = BasicStatistic(document_index, pplx, reference_path=args.ref_model)
+def load_transcript(transcript_name_or_path):
+  try:
+    # First try to load the transcript as a HF dataset.
+    if transcript_name_or_path.count('/') < 2:
+      dataset = load_dataset(transcript_name_or_path, split="train")
+    else:
+      # The dataset might contain subsets.
+      dataset_name, subset_name = transcript_name_or_path.rsplit("/", 1)
+      dataset = load_dataset(dataset_name, subset_name, split="train")
+  except:
+    # Try to load the transcript as a local dataset.
+    dataset = load_from_disk(transcript_name_or_path) 
+  return dataset
+
+
+def phi_qr(args, document_index, metric_fn):
+  phi = BasicStatistic(document_index, metric_fn, reference_path=args.ref_model)
   return phi(args.model)
 
 
 if __name__ == '__main__':  
   parser = argparse.ArgumentParser()
-  parser.add_argument("--model", type=str, default="EleutherAI/pythia-6.9b-deduped")
-  parser.add_argument("--ref_model", type=str, default="EleutherAI/pythia-6.9b")
+  parser.add_argument("--model", type=str,
+                      default="EleutherAI/pythia-6.9b-deduped")
+  parser.add_argument("--ref_model", type=str,
+                      default="EleutherAI/pythia-6.9b")
   parser.add_argument("--n_samples", type=int, default=100000)
-  parser.add_argument("--dataset_name", type=str, default="pythia_deduped_100k")
+  parser.add_argument("--transcript", type=str,
+                      default="hij/sequence_samples/pythia_deduped_100k")
+  parser.add_argument("--metric_column_name", type=str, default=None)
+  parser.add_argument("--ref_metric_column_name", type=str, default=None) 
   args = parser.parse_args()
 
-  hf_sequence_samples = "hij/sequence_samples"
-  dataset = load_dataset(hf_sequence_samples, args.dataset_name, split="train")
+  transcript = load_transcript(args.transcript)
 
-  tokens = list(dataset["tokens"])[:args.n_samples]
-  order = list(dataset["index"])[:args.n_samples]
+  tokens = list(transcript["tokens"])[:args.n_samples]
+  order = list(transcript["index"])[:args.n_samples]
 
   tokenizer = AutoTokenizer.from_pretrained(args.model)
   texts = tokenizer.batch_decode(tokens)
 
   document_index = DocumentIndex(texts, order)
 
-  print(phi_qr(args, document_index))
+  if not args.metric_column_name:
+    # Compute metrics.
+    print(phi_qr(args, document_index, pplx))
+  else:
+    # Use pre-computed metrics.
+    metrics = list(transcript[args.metric_column_name])[:args.n_samples]
+    metrics = np.array([np.mean(x) for x in metrics])
+    ref_metrics = None
+    if args.ref_metric_column_name:
+      ref_metrics = list(
+          transcript[args.ref_metric_column_name])[:args.n_samples]
+      ref_metrics = np.array([np.mean(x) for x in ref_metrics])
+    print(phi_qr(args, document_index,
+                lambda x, y: metrics if x == args.model else ref_metrics))
